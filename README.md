2a1b941634dddeaf42cee7096886761061674a75
Arunsh4h <98868947+Arunsh4h@users.noreply.github.com>
2023-03-14 18:38:55 +0530

    rnd

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # django-html-to-pdf
+
+# Upload edits - python manage.py migrate
+# start app - python manage.py runserver
--- reks/html-to-pdf.py
+++ reks/html-to-pdf.py
@@ -1,91 +1,33 @@
+import asyncio
 import time
 import sys
-import urllib.request
-import os
-import PyPDF2
-from fpdf import FPDF
-
-from pyhtml2pdf import converter
-
-import reportlab
+import io
+from PyPDF2 import PdfFileWriter, PdfFileReader
 from reportlab.lib.units import mm
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
+from pyppeteer import launch
 
-from PyPDF2 import PdfFileWriter, PdfFileReader
+async def generate_pdf(report_url, output_file):
+    browser = await launch()
+    page = await browser.newPage()
+    await page.goto(report_url, waitUntil='networkidle0')
+    
+    # Wait for 5 seconds to ensure that the page has fully loaded
+    await asyncio.sleep(5)
+    
+    pdf_data = await page.pdf(format='A4', printBackground=True)
+    await browser.close()
+    
+    with open(output_file, 'wb') as f:
+        f.write(pdf_data)
 
-print(sys.argv)
-
-output_dir = "."
-json_name = ""
-if len(sys.argv) > 1 :
-    json_name = sys.argv[1]
-    json_name = json_name.replace("\\", "")
-
-timestr = time.strftime("%Y%m%d-%H%M%S")
-
-final_report_output = output_dir + '/Analytics-report'+ timestr + '.pdf'
-report_url = 'http://127.0.0.1:8000/?reportType=long'
-json_url = 'http://localhost:8888/'
-new_json_url = json_url + json_name
-url=urllib.request.urlopen(new_json_url)
-print(len(url.read()))
-
-timeout_value = 20
-if len(url.read()) > 60000 :
-    timeout_value = 200
-
-result = converter.__get_pdf_from_html(report_url, timeout_value, True, print_options={'landscape': False,'paperWidth': 8.27,'paperHeight': 11.69})
-
-with open(final_report_output, "wb") as file:
-    file.write(result)
-
-#basic class with super - to skip parent tempo.
-class NumberPDF(FPDF):
-    def __init__(self, numberOfPages):
-        super(NumberPDF, self).__init__()
-        self.numberOfPages = numberOfPages
-#not header - pass      #self used for first arg
-    def header(self):
-        pass #pass is used for pass - no error - code run
-
-    def footer(self):
-        self.set_y(-12)
-        self.set_font('Arial', 'I', 6)
-        #self.cell(0){self.page_no()}{self.numberOfPages}"'')
-        #self.cell(0, 10, f"{self.page_no()}{self.numberOfPages}", 0, 0,'')
-        self.cell(0, 10, f"Page {self.page_no()} of {self.numberOfPages}", 0, 0, 'R')
-
-
-# Grab the file you want to add pages to
-inputFile = PyPDF2.PdfFileReader(final_report_output)
-outputFile = final_report_output
-
-tempNumFile = NumberPDF(inputFile.getNumPages())
-
-for page in range(inputFile.getNumPages()):
-    tempNumFile.add_page()
-
-tempNumFile.output("tempNumbering.pdf")
-
-mergeFile = PyPDF2.PdfFileReader("tempNumbering.pdf")
-
-mergeWriter = PyPDF2.PdfFileWriter()
-
-# Loop through the page - Temporary numbering
-for x, page in enumerate(mergeFile.pages):
-    # Get page input 1
-    inputPage = inputFile.getPage(x)
-    # Merge the input and Temporary numbering
-    inputPage.mergePage(page)
-    # Add the merged to the final_report_output
-    mergeWriter.addPage(inputPage)
-
-# To Delete temporary file
-os.remove("tempNumbering.pdf")
-
-#merged output - write
-with open(outputFile, 'wb') as fh:
-    mergeWriter.write(fh)
-
+if __name__ == '__main__':
+    output_dir = "."
+    if len(sys.argv) > 1:
+        output_dir = sys.argv[1]
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    final_report_output = output_dir + '/Analytics-report'+ timestr + '.pdf'
+    report_url = 'http://localhost:8888/'
+    asyncio.get_event_loop().run_until_complete(generate_pdf(report_url, final_report_output))
